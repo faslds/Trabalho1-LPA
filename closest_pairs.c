b9e366f8781c817f533621c66ee79550bcd38bb6
faslds <felipe.senger.souza@usp.br>
2015-09-21 19:06:39 -0300

    Closest Pairs funciona com números negativos.
    O programa foi otimizado e agora funciona com entradas negativas. Ainda
    são necessários testes, e o código não está devidamente otimizado e
    comentado.

--- closest_pairs.c
+++ closest_pairs.c
@@ -1,8 +1,24 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<math.h>
 
-closestpair(int vet[], int n) //funcao para encontrar os dois pares com a menor diferenca
+void bubble(int vet[], int n) //funcao para ordenar o vetor
+{
+	int a, j, i; 
+	for(j=0; j<n-j; j++) //percorre o vetor 
+	{
+		for (i=0; i<n-j-1;i++) //percorre o vetor e compara cada elemento com o do seu lado direito (vet[i+1]). Se o elemento da direita for menor, troca os dois de lugar
+		{
+			if (vet[i+1]<vet[i])
+			{
+			a=vet[i+1];
+			vet[i+1]=vet[i];
+			vet[i]=a; //feita a troca
+			}
+		}
+	}
+}
+
+/*closestpair(int vet[], int n) //funcao para encontrar os dois pares com a menor diferenca
 {
 	int a, b, c, i, j, menor;
 	
@@ -47,6 +63,22 @@ closestpair(int vet[], int n) //funcao para encontrar os dois pares com a menor 
 		}
 	}
 	printf("%d e %d, nas posicoes %d e %d respectivamente. Sua diferenca eh %d.\n", vet[a], vet[b], a, b, menor);
+}*/
+
+closestpair(int vet[], int n)
+{
+	int i, posicao=0, dif, c;
+	dif=vet[posicao + 1] - vet[posicao]; //dif eh inicializada com a diferenca entre os dois primeiros elementos.
+	for (i=1; i<n-1; i++)
+	{
+		c=vet[i+1] - vet[i]; //a partir do elemnto na posicao 1, cada par eh comparado
+		if (c < dif)
+		{
+			dif = c;//encontra diferencas menores que a dos dois pimeiros
+			posicao = i;//armazena tambem a posicao dos numeros com a menor diferenca
+		}
+	}
+	printf("%d e %d, nas posicoes %d e %d respectivamente. Sua diferenca eh %d.", vet[posicao], vet[posicao + 1], posicao, posicao +1, dif);
 }
 
 int main()
@@ -58,6 +90,7 @@ int main()
 	printf("Digite os numeros: ");
 	for (i=0; i<n; i++)//esse for preenche o vetor
 		scanf("%d", &vet[i]);
+	bubble(vet, n); //ordena o vetor
 	printf("\nOs dois elementos com a menor diferenca sao:\n");
 	closestpair(vet, n);//chama a funcao que imprime os dois numeros com a menor diferenca
 	free(vet);//libera a memoria alocada
