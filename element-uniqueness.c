2088b87c6fb43d9c589f735ca4118376bfd82ba4
faslds <felipe.senger.souza@usp.br>
2015-09-22 11:15:54 -0300

    Erro corrigido
    Corrigido erro quando era entrado apenas 1 número.

--- element-uniqueness.c
+++ element-uniqueness.c
@@ -8,7 +8,8 @@ int eleunique(int vet[], int n) //funcao para checar quais elemntos sao unicos
 	if (vet[0] != vet[1]) //checa se o primeiro eh unico (porque voce nao vai poder comparar com vet[-1])
 	{
 	printf("\n%d", vet[0]);
-	a++; //"a" e usada para rastrear a existencia elemntos unicos. Se nenhuma das trocas for feita, a nao sera incrementada e sera igual a 0, fazendo a mensagem "Nao exsite um elemento unico" ser impressa 
+	a++; /*"a" e usada para rastrear a existencia elemntos unicos. Se nenhuma das trocas for feita,
+	a nao sera incrementada e sera igual a 0, fazendo a mensagem "Nao exsite um elemento unico" ser impressa*/
 	}
 	for (i=1; i<n-1; i++)
 	{
@@ -18,7 +19,7 @@ int eleunique(int vet[], int n) //funcao para checar quais elemntos sao unicos
 		a++;
 		}
 	}
-	if (vet[n-1] != vet[n-2]) //checa se o ultimo eh unico
+	if (vet[n-1] != vet[n-2] && n>1) //checa se o ultimo eh unico
 	{
 	printf("\n%d", vet[n-1]);
 	a++;
@@ -35,7 +36,8 @@ void bubble(int vet[], int n) //funcao para ordenar o vetor
 	int a, j, i; 
 	for(j=0; j<n-j; j++) //percorre o vetor 
 	{
-		for (i=0; i<n-j-1;i++) //percorre o vetor e compara cada elemento com o do seu lado direito (vet[i+1]). Se o elemento da direita for menor, troca os dois de lugar
+		for (i=0; i<n-j-1;i++) /*percorre o vetor e compara cada elemento com o do seu lado direito (vet[i+1]).
+		Se o elemento da direita for menor, troca os dois de lugar*/
 		{
 			if (vet[i+1]<vet[i])
 			{
